57acaa488e936e9e3b9c9010e6365ad32f6be926
haru <godjaewook@naver.com>
2024-05-22 00:53:28 +0900

    bottom 맨아래에 문제가 있는듯

--- lib/screens/sign_up/sign_up_screen.dart
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -6,11 +8,56 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(
+            children: [
+              Gaps.v80,
+              Text(
+                'Sign up for TikTok',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              Text(
+                "Create a profile, follow other accounts, make your own videos, and more.",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Sign Up Screen'),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey[200],
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Already have an account?",
+              ),
+              Gaps.h5,
+              Text(
+                "Log in",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
