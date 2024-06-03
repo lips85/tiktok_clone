266a18e73ef5add971fc088c7699672e4ab63106
haru <88703915+lips85@users.noreply.github.com>
2024-06-03 16:01:27 +0900

    중간

--- lib/02_tweeter/features/authentication/login_page_tweet.dart
+++ lib/02_tweeter/features/authentication/login_page_tweet.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 
@@ -18,26 +24,119 @@ class LoginPageTweet extends StatelessWidget {
           color: Color(0xff4E98E9),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: Sizes.size24,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size32,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Gaps.v80,
-            Gaps.v80,
-            Expanded(
-              child: Text(
-                "See what's happening in the world right now.",
-                style: TextStyle(
-                  fontSize: Sizes.size32,
-                  fontWeight: FontWeight.w900,
-                ),
+            Gaps.v96,
+            const Text(
+              "See what's happening in the world right now.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: Sizes.size32,
+                fontWeight: FontWeight.w900,
               ),
+            ),
+            Gaps.v96,
+            Column(
+              children: [
+                const LoginFormButton(
+                  assetPath: 'assets/images/google.svg',
+                  text: "Continue with Google",
+                  white: true,
+                ),
+                Gaps.v20,
+                const LoginFormButton(
+                  assetPath: 'assets/images/apple.svg',
+                  text: "Continue with Apple",
+                  white: true,
+                ),
+                Gaps.v20,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                    Gaps.h16,
+                    const Text(
+                      "or",
+                      style: TextStyle(
+                        fontSize: Sizes.size10,
+                      ),
+                    ),
+                    Gaps.h16,
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                const LoginFormButton(
+                  text: "Create account",
+                  white: false,
+                ),
+              ],
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class LoginFormButton extends StatelessWidget {
+  final String? assetPath;
+  final String text;
+  final bool white;
+
+  const LoginFormButton({
+    super.key,
+    this.assetPath,
+    required this.text,
+    required this.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 320,
+      height: 50,
+      decoration: BoxDecoration(
+        color: white ? Colors.white : Colors.black,
+        border: Border.all(
+          color: Colors.grey.shade300,
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(Sizes.size40),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (assetPath != null)
+            SvgPicture.asset(
+              assetPath!,
+              fit: BoxFit.cover,
+              width: Sizes.size24,
+              height: Sizes.size24,
+            ),
+          if (assetPath != null) Gaps.h24,
+          Text(
+            text,
+            style: TextStyle(
+                fontSize: Sizes.size20,
+                fontWeight: FontWeight.w900,
+                color: white ? Colors.black : Colors.white),
+          )
+        ],
       ),
     );
   }
