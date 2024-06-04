edbe7b226875d266f2d27112edb1aac1a60cd27a
haru <godjaewook@naver.com>
2024-06-04 22:20:25 +0900

    init 2

--- lib/02_tweeter/features/authentication/login_page_tweet.dart
+++ lib/02_tweeter/features/authentication/login_page_tweet.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:tiktok_clone/02_tweeter/features/authentication/Create_account_tweet.dart';
+import 'package:tiktok_clone/02_tweeter/features/authentication/create_account_tweet.dart';
 import 'package:tiktok_clone/02_tweeter/features/widgets/login_form_button.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
