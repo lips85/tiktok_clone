103430ba346b8ae0d183439546638d70973adb41
haru <godjaewook@naver.com>
2024-06-13 00:29:42 +0900

    7.3까지 진행

--- lib/01_tiktok/features/videos/video_timeline_screen.dart
+++ lib/01_tiktok/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/01_tiktok/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -8,29 +9,47 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
+  final PageController _pageController = PageController();
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
+
+  void _onPageChanged(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      setState(() {});
+    }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override //controller 가 있으면 dispose()를 호출해야 한다.
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: [
-        Container(
-          color: Colors.red,
-          child: const Center(
-            child: Text("Video Timeline"),
-          ),
-        ),
-        Container(
-          color: Colors.blue,
-          child: const Center(
-            child: Text("Following"),
-          ),
-        ),
-        Container(
-          color: Colors.green,
-          child: const Center(
-            child: Text("For You"),
-          ),
-        ),
-      ],
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
+      ),
     );
   }
 }
